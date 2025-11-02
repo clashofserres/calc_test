ce6dd409faa43c640099b8df8c10df8c41a7b17c
drmqwerty <arson240sx@gmail.com>
2025-11-02 19:35:34 +0200

    MultCalcClass

---
+++ src/main/java/clashofserres/calc/MultCalcClass.java
@@ -0,0 +1,11 @@
+package clashofserres.calc;
+import clashofserres.calc.IBaseCalcClass;
+
+public class MultCalcClass implements IBaseCalcClass
+{
+    @Override
+    public double calculate(double a, double b)
+    {
+        return a * b;
+    }
+}
